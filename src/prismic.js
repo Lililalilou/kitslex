1ea3e876e10ff5e7f66aed0d40bd8de3d7778bd0
Nelly Martin <schott.nelly@gmail.com>
2022-09-23 17:55:32 +0200

    Prismic key handling

--- src/prismic.js
+++ src/prismic.js
@@ -5,8 +5,7 @@ export const repositoryName = 'kitslex'
 
 export const client = prismic.createClient(repositoryName, {
   // If your repository is private, add an access token
-  accessToken:
-    'MC5ZdkVsTXhBQUFDa0EzR0tM.Uu-_vTzvv73vv70WdAZ8SG7vv73vv73vv73vv73vv70z77-977-9au-_vT8j77-9by_vv73vv73vv71JLe-_vQ',
+  accessToken: 'process.env.PRISMIC_APP_ACCESS_TOKEN',
 
   // This defines how you will structure URL paths in your project.
   // Update the types to match the Custom Types in your project, and edit
